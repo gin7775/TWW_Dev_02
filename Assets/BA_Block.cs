2c80d4980279a897e84b6ec35c711895ae2aeec2
gin7775 <103499625+gin7775@users.noreply.github.com>
2023-12-11 21:28:52 +0100

    Fase1 boss

---
+++ Assets/BA_Block.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BA_Block : StateMachineBehaviour
+{
+    public float blockTimer = 4;
+    private ArmoreBoss armore;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        armore = animator.gameObject.GetComponent<ArmoreBoss>();
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        armore.guardState = 2;
+        blockTimer -= Time.deltaTime;
+        if (blockTimer <= 0)
+        {
+            animator.SetTrigger("CounterAtack");
+        }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        armore.guardState = 1;
+        blockTimer = 4;
+    }
+
+}
