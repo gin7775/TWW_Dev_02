7638e069a1a3b55060c1b63f251c7825c1848014
gin7775 <103499625+gin7775@users.noreply.github.com>
2024-01-16 23:25:26 +0100

    Armore stopit

--- Assets/BA_Shoot.cs
+++ Assets/BA_Shoot.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BA_Shoot : StateMachineBehaviour
 {
     public float returnTimer = 5;
+    public float speed;
     private ArmoreBoss armore;
     public GameObject player;
-    private UnityEngine.AI.NavMeshAgent armoreAgent;
+    private NavMeshAgent armoreAgent;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,12 +17,14 @@ public class BA_Shoot : StateMachineBehaviour
         armore = animator.gameObject.GetComponent<ArmoreBoss>();
         player = armore.player;
         armore.AnimArmoreShoot();
+        speed = armoreAgent.speed;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.transform.LookAt(player.transform.position);
+        armoreAgent.speed = 0;
         returnTimer -= Time.deltaTime;
         if (returnTimer <= 0)
         {
@@ -31,6 +35,7 @@ public class BA_Shoot : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        armoreAgent.speed = speed;
         returnTimer = 5;
     }
 }
