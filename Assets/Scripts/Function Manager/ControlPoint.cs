a910304864ab5006972c696e4a807a498286fc52
gin7775 <103499625+gin7775@users.noreply.github.com>
2024-03-14 17:02:04 +0100

    Spawn Point pulido

--- Assets/Scripts/Function Manager/ControlPoint.cs
+++ Assets/Scripts/Function Manager/ControlPoint.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class ControlPoint : MonoBehaviour
 {
+    public bool canTriger;
     public SceneInfo SceneInfo;
     //Se usa para decirle al scene info cual es este punto 
     public int pointIndex;
 
     private void Start()
     {
+        canTriger = true;
         //SceneInfo = FindObjectOfType<SceneInfo>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player")&& canTriger == true)
         {
             SceneInfo.controlPoint = pointIndex;
             Debug.Log("El punto de control es " + SceneInfo.controlPoint);
-            Destroy(this.gameObject, 1.3f);
+            canTriger=false;
         }
         
     }
