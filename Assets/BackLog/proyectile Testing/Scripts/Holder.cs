0e76e9d8fcbba4e197cca539b51e0498572d91a6
gin7775 <alfonsogianmarco@gmail.com>
2024-05-08 04:16:32 +0200

    Conseguido suuuu

--- Assets/BackLog/proyectile Testing/Scripts/Holder.cs
+++ Assets/BackLog/proyectile Testing/Scripts/Holder.cs
@@ -1,29 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Holder : MonoBehaviour
 {
     public GameObject circleProyectile;
-    public int circleNumberMax,circleCount;
-    public GameObject[] circles;
+    public int circleNumberMax, circleCount;
+    public List<GameObject> circles; // Cambiamos de arreglo a lista
 
     public GameObject Player;
     public Gun_Test[] Guns;
     public bool needPlayer;
+
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
 
         Guns = GetComponentsInChildren<Gun_Test>();
-        
-        if(needPlayer == true && Player != null)
+
+        if (needPlayer && Player != null)
         {
-            this.transform.LookAt(Player.transform.position);
+            transform.LookAt(Player.transform.position);
         }
-        
+
+        circles = new List<GameObject>(); // Inicializamos la lista
     }
-    public void SetGun(int shoots, float fireRate, float speed,bool track)
+
+    public void SetGun(int shoots, float fireRate, float speed, bool track)
     {
         for (int i = 0; i < Guns.Length; i++)
         {
@@ -33,6 +37,7 @@ public class Holder : MonoBehaviour
             Guns[i].toPlayer = track;
         }
     }
+
     public void StartShoot()
     {
         for (int i = 0; i < Guns.Length; i++)
@@ -40,24 +45,27 @@ public class Holder : MonoBehaviour
             Guns[i].cantShoot = false;
         }
     }
+
     public void CircleTurrets()
     {
         for (int i = 0; i < Guns.Length; i++)
         {
             GameObject x = Instantiate(circleProyectile, Guns[i].transform.position, Quaternion.identity);
-            circles[i] = x;
-            circles[i].GetComponent<Gunner_Test>().secondFace = false;
-            circles[i].GetComponent<Gunner_Test>().CircleGun();
+            circles.Add(x); // Agregamos el nuevo círculo a la lista
+            x.GetComponent<Gunner_Test>().proyectilePos = Guns[i].transform.position;
+            x.GetComponent<Gunner_Test>().secondFace = false;
+            x.GetComponent<Gunner_Test>().CircleGun();
         }
         circleCount++;
         if (circleCount >= circleNumberMax)
         {
             Debug.Log("shouldDestroy");
-            for (int i = 0; i < Guns.Length; i++)
+            for (int i = 0; i < circles.Count; i++) // Iteramos sobre la lista
             {
-
-                Destroy(circles[i]);
+                Destroy(circles[i]); // Destruimos cada círculo en la lista
             }
+            circles.Clear(); // Limpiamos la lista después de destruir los círculos
+            Destroy(this.gameObject);
         }
     }
 }
