ec2ca1cc25e94b35eb0c6e50baa794a495690885
gin7775 <103499625+gin7775@users.noreply.github.com>
2024-05-09 16:26:21 +0200

    agua resuelta en teoria

---
+++ Assets/_VerticalSlice/Scripts/FunctionManager/DownLimitDeath.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownLimitDeath : MonoBehaviour
+{
+    public GameObject player;
+    public PlayerStats playerStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerStats = player.GetComponent<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public IEnumerator MirarDistancia() 
+    {
+        if (player != null)
+        {
+            if (player.transform.position.y <= this.transform.position.y - 10) 
+            {
+
+
+                if (playerStats != null)
+                {
+                    playerStats.TakeDamage(10000);
+                    Debug.Log("Has tocado");
+                }
+
+            }
+        }
+        yield return new WaitForSeconds(3);
+
+    }
+}
